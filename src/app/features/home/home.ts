d422ef515c3f12f90c103a6c1b0718206881098b
CarlosEduardS <carloseduardo.s.a279@gmail.com>
2026-04-29 18:30:19 -0300

    feat: update home layout with card component and adjust main interface structure

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutMainInterface } from "../../shared/layout/layout-main-interface/layout-main-interface";
+import { Card } from "../../shared/ui/card/card";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [LayoutMainInterface],
+  imports: [LayoutMainInterface, Card, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
