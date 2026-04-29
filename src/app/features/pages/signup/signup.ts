1d3f8eb8ca401c291e84c991fa5e0bef2d9f89fc
CarlosEduardS <carloseduardo.s.a279@gmail.com>
2026-04-29 08:44:40 -0300

    feat: implement signup page with form validation and navigation

---
+++ src/app/features/pages/signup/signup.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayout } from "../../layout/default-login-layout/default-login-layout";
+import { FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+import { PrimaryInput } from "../../components/primary-input/primary-input";
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login';
+import { ToastrService } from 'ngx-toastr';
+import e from 'express';
+
+interface SignupForm {
+  name: FormControl;
+  email: FormControl;
+  password: FormControl;
+  passwordConfirme: FormControl;
+
+}
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  providers: [LoginService],
+  imports: [DefaultLoginLayout, ReactiveFormsModule, PrimaryInput],
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
+})
+export class Signup {
+  SignupForm!: FormGroup<SignupForm>;
+  errorMessage = '';
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
+    this.SignupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirme: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit(){
+    if (this.SignupForm.value.passwordConfirme !== this.SignupForm.value.password) {
+      this.errorMessage = 'As senhas não coincidem.';
+    } else {
+      this.errorMessage = 'Erro ao criar conta. Tente novamente mais tarde.';
+    }
+    this.loginService.login(this.SignupForm.value.email!, this.SignupForm.value.password!).subscribe({
+      next: () => this.toastService.success('Conta criada com sucesso!'),
+      error: () => this.toastService.error(this.errorMessage)
+    })
+  }
+
+  navigate(){
+    this.router.navigate(['/login'])
+  }
+}
