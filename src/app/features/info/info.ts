1ee6bc2ae60353175d2c67a87f7b68cb9b0800be
CarlosEduardS <carloseduardo.s.a279@gmail.com>
2026-05-07 08:09:09 -0300

    feat: adicionar conteúdo informativo sobre Arduino e engenharia; ajustar layout e estilo da seção

--- src/app/features/info/info.ts
+++ src/app/features/info/info.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LayoutMainInterface } from "../../shared/layout/layout-main-interface/layout-main-interface";
+import { Card } from "../../shared/ui/card/card";
 
 @Component({
   selector: 'app-info',
-  imports: [LayoutMainInterface],
+  imports: [LayoutMainInterface, Card],
   templateUrl: './info.html',
   styleUrl: './info.scss',
 })
