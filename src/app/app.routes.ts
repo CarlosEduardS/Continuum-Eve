d53da2f54cf7b142b6d56eecaa8139e4fade0d82
CarlosEduardS <carloseduardo.s.a279@gmail.com>
2026-06-15 08:37:27 -0300

    feat: adicionar rota para página de redefinição de senha e ajustar estilos do footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Info } from './features/info/info';
 import { Contato } from './features/contato/contato';
 import { Controller } from './features/controller/controller';
 import { Profile } from './features/profile/profile';
+import { AlteSenha } from './features/pages/alte-senha/alte-senha';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
@@ -15,5 +16,6 @@ export const routes: Routes = [
     {path: 'contact', component: Contato},
     {path: 'controller', component: Controller},
     {path: 'profile', component: Profile},
+    {path: 'password-reset', component: AlteSenha},
     {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
